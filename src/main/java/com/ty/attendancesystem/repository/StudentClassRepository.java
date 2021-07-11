af630fd3c7519581228b2e24c7c8a1d11082328d
Tuan Tran <tuantrananhit@gmail.com>
2021-07-11 23:23:28 +0700

    Api lấy danh sách học sinh theo lớp học (cần trước) - Done Api lay chi tiet phu huynh theo ma sinh vien (chỗ api lấy chi tiet sv) - Done

--- src/main/java/com/ty/attendancesystem/repository/StudentClassRepository.java
+++ src/main/java/com/ty/attendancesystem/repository/StudentClassRepository.java
@@ -3,5 +3,9 @@ package com.ty.attendancesystem.repository;
 import com.ty.attendancesystem.model.StudentClass;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudentClassRepository extends JpaRepository<StudentClass, String> {
+
+    List<StudentClass> getStudentClassByClassId(String classId);
 }
