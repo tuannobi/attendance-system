51cd1855fb80a6f532bc52e72b1ed402875825f3
Tuan Tran <tuantrananhit@gmail.com>
2021-07-14 20:27:12 +0700

    add Student To class and import excel

--- src/main/java/com/ty/attendancesystem/repository/StudentClassRepository.java
+++ src/main/java/com/ty/attendancesystem/repository/StudentClassRepository.java
@@ -2,10 +2,14 @@ package com.ty.attendancesystem.repository;
 
 import com.ty.attendancesystem.model.StudentClass;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface StudentClassRepository extends JpaRepository<StudentClass, String> {
 
     List<StudentClass> getStudentClassByClassId(String classId);
+
+    @Query(value = "delete from student_class where student_user_id = ?1 and class_id = ?2", nativeQuery = true)
+    int deleteStudentFromClassById(String studentId, String classId);
 }
--- src/main/java/com/ty/attendancesystem/service/TimeTableCourseService.java
+++ src/main/java/com/ty/attendancesystem/service/TimeTableCourseService.java
@@ -11,4 +11,5 @@ public interface TimeTableCourseService extends BaseService<TimeTableCourse, Lon
     TimeTableCourse insert(TimeTableCourse timeTableCourse);
 
     TimeTableCourse update(TimeTableCourse timeTableCourse);
+    String getStudentIdFromTableCourse(Long timeTableCourse);
 }
