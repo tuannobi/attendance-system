12ec0bb8fec5d3df3fb1729a5f166b55dde49dcc
Tuan Tran <tuantrananhit@gmail.com>
2021-07-20 00:09:40 +0700

    update report

--- src/main/java/com/ty/attendancesystem/repository/AttendanceDetailRepository.java
+++ src/main/java/com/ty/attendancesystem/repository/AttendanceDetailRepository.java
@@ -43,9 +43,9 @@ public interface AttendanceDetailRepository extends CustomRepository<AttendanceD
 
     List<AttendanceDetail> getAttendanceDetailsByClazz_Id(String classId);
 
-    @Query(value = "select count (*) from attendance_detail join users on attendance_detail.student_user_id = users.id where attendance_detail.status = 1 and class_id = :classId and attendance_detail.student_user_id = :studentId",nativeQuery = true)
+    @Query(value = "select count (*) from attendance_detail where status = 1 and class_id = :classId and student_user_id = :studentId",nativeQuery = true)
     int countStatusIsPresentByStudentByClass(String classId, String studentId);
 
-    @Query(value = "select count (*) from attendance_detail join users on attendance_detail.student_user_id = users.id where attendance_detail.status = 0 and class_id = :classId and attendance_detail.student_user_id = :studentId", nativeQuery = true)
+    @Query(value = "select count (*) from attendance_detail where status = 0 and class_id = :classId and student_user_id = :studentId", nativeQuery = true)
     int countStatusIsAbsentByStudentByClass(String classId, String studentId);
 }
