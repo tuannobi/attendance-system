4ebbbf5f20452d260b19f882df5abeef617829d6
Tuan Tran <tuantrananhit@gmail.com>
2021-07-20 00:37:43 +0700

    fix report

--- src/main/java/com/ty/attendancesystem/controller/ReportRestController.java
+++ src/main/java/com/ty/attendancesystem/controller/ReportRestController.java
@@ -52,6 +52,7 @@ public class ReportRestController {
             int absent = attendanceDetailService.countStatusIsAbsentByStudentByClass(classId,tempUser.get().getId());
             student.setAbsent(present);
             students.add(student);
+            //
             totalPresent += present;
             totalAbsent += absent;
         }
